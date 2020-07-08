e1c3db9d913731685bbc78b15593c80e885f118d
pquiroza <pedro.quiroz@gmail.com>
2020-07-08 12:48:20 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,16 +9,5 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-production:false,
-server: "http://192.168.0.29:5002/ligueros/api",
-firebase:{
-  apiKey: "AIzaSyAeE1UZ4_hxQpG9M85iT9HFQDuuJPnfFv4",
-     authDomain: "ligueros-af5b3.firebaseapp.com",
-     databaseURL: "https://ligueros-af5b3.firebaseio.com",
-     projectId: "ligueros-af5b3",
-     storageBucket: "ligueros-af5b3.appspot.com",
-     messagingSenderId: "290557695821",
-     appId: "1:290557695821:web:62d5f7e53cc626b0648904",
-     measurementId: "G-VFK6CNBKME"
-   }
+
 };
